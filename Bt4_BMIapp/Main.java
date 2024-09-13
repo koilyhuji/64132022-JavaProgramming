d247564b40c54e60a03fe63012d4355aed17502b
bobby-stanoff <hongnguyen210180@gmail.com>
2024-09-13 04:22:01 -0400

    add race check

--- Bt4_BMIapp/Main.java
+++ Bt4_BMIapp/Main.java
@@ -5,21 +5,44 @@ public class Main {
 
         float heightInMeters, weight;
         float bmi;
+        String race = "";
         Scanner scan = new Scanner(System.in);
+        System.out.println("CHUONG TRINH TINH BMI");
+        System.out.println("---------------------");
+        System.out.print("Ban co phai la nguoi chau a? (y/n): ");
+        race = scan.nextLine();
+        while (race != "y" || race != "n"){
+            System.out.println("vui long chon y/n");
+            System.out.print("Ban co phai la nguoi chau a? (y/n): ");
+            race = scan.nextLine();
+        }
         System.out.print("Nhap chieu cao (m): ");
         heightInMeters = scan.nextFloat();
         System.out.print("Nhap can nang: ");
         weight = scan.nextFloat();
         bmi = weight / (heightInMeters * heightInMeters);
         System.out.println("BMI: "+ String.format("%.2f", bmi));
-        if (bmi < 18.5) {
-            System.out.println("Underweight");
-        } else if (bmi >= 18.5 && bmi < 25) {
-            System.out.println("Normal weight");
-        } else if (bmi >= 25 && bmi < 30) {
-            System.out.println("Overweight");
-        } else {
-            System.out.println("Obese");
+        if(race == "n"){
+            if (bmi < 18.5) {
+                System.out.println("Underweight");
+            } else if (bmi >= 18.5 && bmi < 25) {
+                System.out.println("Normal weight");
+            } else if (bmi >= 25 && bmi < 30) {
+                System.out.println("Overweight");
+            } else {
+                System.out.println("Obese");
+            }
+        }
+        else if (race == "y"){
+            if (bmi < 18.5) {
+                System.out.println("Underweight");
+            } else if (bmi >= 18.5 && bmi < 22.9) {
+                System.out.println("Normal weight");
+            } else if (bmi >= 23 && bmi < 24.9) {
+                System.out.println("Overweight");
+            } else {
+                System.out.println("Obese");
+            }
         }
 
     }
