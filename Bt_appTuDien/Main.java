01adff8c92438a8d55f33f4f75a08124a997099b
koilyhuji <quynh.pg.64cntt@ntu.edu.vn>
2024-10-01 03:38:25 -0400

    them chuc nang doc cau van

--- Bt_appTuDien/Main.java
+++ Bt_appTuDien/Main.java
@@ -9,7 +9,7 @@ public class Main {
         HashMap<String,String> words = new HashMap<String, String>();
         words.put("hello", "chao");
         words.put("world", "the gioi");
-        words.put("one ","mot");
+        words.put("one","mot");
         words.put("two","hai");
         words.put("three","ba");
         words.put("four","bon");
@@ -19,11 +19,23 @@ public class Main {
         words.put("eight","tam");
         System.out.println("nhap tu :");
         Scanner sc = new Scanner(System.in);
-        String w = sc.next();
-        if(!words.containsKey(w)){
-            System.out.println("tu nhap khong co trong tu dien");
-            return;
+        String w = sc.nextLine();
+        System.out.println(w);
+        String[] tokens = w.split("\\s+");
+        String res = "";
+        for(int i = 0 ; i< tokens.length; i++){ 
+            if(!words.containsKey(tokens[i])){
+                res += tokens[i] + " ";
+                continue;
+            }
+            else res += (words.get(tokens[i]) + " ");
         }
-        System.out.println("nghia la : "+ words.get(w));
+        System.out.println("nghia la : "+ res);
+        return;
+        // if(!words.containsKey(w)){
+        //     System.out.println("tu nhap khong co trong tu dien");
+        //     return;
+        // }
+        // System.out.println("nghia la : "+ words.get(w));
     }
 }
