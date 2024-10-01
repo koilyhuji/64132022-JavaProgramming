ec2cd3872aafcc42941ebe71d7ff244c7bfdc35c
koilyhuji <quynh.pg.64cntt@ntu.edu.vn>
2024-10-01 03:06:35 -0400

    done

---
+++ Bt_appTuDien/Main.java
@@ -0,0 +1,27 @@
+package Bt_appTuDien;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        HashMap<String,String> words = new HashMap<String, String>();
+        words.put("hello", "chao");
+        words.put("world", "the gioi");
+        words.put("one ","mot");
+        words.put("two","hai");
+        words.put("three","ba");
+        words.put("four","bon");
+        words.put("five","nam");
+        words.put("six","sau");
+        words.put("seven","bay");
+        words.put("eight","tam");
+        System.out.println("nhap tu :");
+        Scanner sc = new Scanner(System.in);
+        String w = sc.next();
+        System.out.println("nghia la : "+ words.get(w));
+    }
+}
