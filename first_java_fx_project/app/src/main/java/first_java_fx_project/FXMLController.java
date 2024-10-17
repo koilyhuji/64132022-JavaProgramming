0142762e44fd57804d79545ce89f2bcf9d33a2a8
koilyhuji <quynh.pg.64cntt@ntu.edu.vn>
2024-10-17 01:14:39 -0400

    change gradle version

--- first_java_fx_project/app/src/main/java/first_java_fx_project/FXMLController.java
+++ first_java_fx_project/app/src/main/java/first_java_fx_project/FXMLController.java
@@ -9,17 +9,17 @@ import javafx.scene.control.Label;
 
 public class FXMLController {
 
-    // @FXML
-    // private Label label;
-
-    // public void initialize() {
-    //     String javaVersion = System.getProperty("java.version");
-    //     String javafxVersion = System.getProperty("javafx.version");
-    //     label.setText("Hello, JavaFX anh tai " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
-    // }
-    // public void add(){
-    //     JSONObject jobject = new JSONObject();
-    //     jobject.put("ha", 12);
-
-    // }
+//    @FXML
+//    private Label label;
+//
+//    public void initialize() {
+//        String javaVersion = System.getProperty("java.version");
+//        String javafxVersion = System.getProperty("javafx.version");
+//        label.setText("Hello, JavaFX anh tai " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
+//    }
+//    public void add(){
+//        JSONObject jobject = new JSONObject();
+//        jobject.put("ha", 12);
+//
+//    }
 }
