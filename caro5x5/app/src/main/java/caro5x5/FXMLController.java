dde2e3a9c9228cdfd7fc9142a5e0d0a456a394e0
koilyhuji <quynh.pg.64cntt@ntu.edu.vn>
2024-10-22 05:02:51 -0400

    finished demo database connection

--- caro5x5/app/src/main/java/caro5x5/FXMLController.java
+++ caro5x5/app/src/main/java/caro5x5/FXMLController.java
@@ -2,7 +2,6 @@ package caro5x5;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 
 public class FXMLController {
