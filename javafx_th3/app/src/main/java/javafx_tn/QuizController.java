d0caeb024b0672dacbd5e75976ad6a10edeb8213
koilyhuji <quynh.pg.64cntt@ntu.edu.vn>
2024-12-06 03:30:10 -0500

    change a,b,c,d button

--- javafx_th3/app/src/main/java/javafx_tn/QuizController.java
+++ javafx_th3/app/src/main/java/javafx_tn/QuizController.java
@@ -11,7 +11,8 @@ public class QuizController {
 
     @FXML
     private VBox buttonVboxCH;
-
+    @FXML
+    private Button btnAnsA,btnAnsB,btnAnsC,btnAnsD;
     public void initialize() {
        
     }
@@ -32,5 +33,14 @@ public class QuizController {
 
         Button clickedButton = (Button) event.getSource();
         clickedButton.setStyle("-fx-background-color: #FF9800; -fx-text-fill: black; -fx-min-width: 150px; -fx-min-height: 30px; -fx-font-size: 14px; -fx-cursor: hand;");
+    }
+
+    public void handleButtonAnsClick(ActionEvent event){
+        btnAnsA.setStyle("-fx-background-color: #1565C0;-fx-text-fill: white;-fx-min-width: 370px;-fx-min-height: 40px;-fx-font-size: 16px;-fx-cursor: hand;-fx-border-color: black;  -fx-border-width: 1px;");
+        btnAnsB.setStyle(btnAnsA.getStyle().toString());
+        btnAnsC.setStyle(btnAnsA.getStyle().toString());
+        btnAnsD.setStyle(btnAnsA.getStyle().toString());
+        Button clickedButton = (Button) event.getSource();
+        clickedButton.setStyle("-fx-background-color: #4FC978;-fx-text-fill: white;-fx-min-width: 370px;-fx-min-height: 40px;-fx-font-size: 16px;-fx-cursor: hand;-fx-border-color: black;  -fx-border-width: 1px;");
     }
 }
